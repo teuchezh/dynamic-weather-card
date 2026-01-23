122885a45ccaad167d965d2095895e1b1aad68ec
Czirják Tamás <tamas.czirjak@gmail.com>
2026-01-23 09:45:15 +0100

    Update translation.ts

--- src/internationalization/locales/en/translation.ts
+++ src/internationalization/locales/en/translation.ts
@@ -66,6 +66,8 @@ const translation: Translation = {
     'language_nl': 'Dutch',
     'language_fr': 'French',
     'language_es': 'Spanish',
+    'language_it': 'Italian',
+    'language_hu': 'Hungarian',
     'wind_speed_unit': 'Wind Speed Unit',
     'wind_speed_unit_ms': 'm/s',
     'wind_speed_unit_kmh': 'km/h'
@@ -147,7 +149,8 @@ const translation: Translation = {
       'german': 'German',
       'dutch': 'Dutch',
       'spanish': 'Spanish',
-      'italian': 'Italian'
+      'italian': 'Italian',
+      'hungarian': 'Hungarian'
     }
   }
 };
